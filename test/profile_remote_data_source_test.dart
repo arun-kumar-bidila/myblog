0db7a9c302fcdd05403a980d66c494063a473510
Arun Kumar Bidila <arunkumarbidila@gmail.com>
2026-04-08 15:31:10 +0530

    test:add fetch all blogs test

--- test/profile_remote_data_source_test.dart
+++ test/profile_remote_data_source_test.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:myblog/core/error/exceptions.dart';
+import 'package:myblog/features/blog/data/models/blog_model.dart';
 import 'package:myblog/features/profile/data/datasources/profile_remote_datasource.dart';
 
 class MockDio extends Mock implements Dio {}
@@ -57,6 +58,61 @@ void main() {
           currentPassword: currentPassword,
           newPassword: newPassword,
         );
+
+        expect(result, throwsA(isA<ServerException>()));
+      });
+    });
+
+    group("fetch blogs by user -", () {
+      test("return a list of blogs when status 200", () async {
+        final mockResponse = Response(
+          requestOptions: RequestOptions(path: ""),
+          statusCode: 200,
+          data: {
+            "blogs": {
+              {
+                "_id": "some",
+                "title": "my blog",
+                "content": "this is my blog content",
+                "imageUrl":
+                    "https://res.cloudinary.com/duoenlwuj/image/upload/v1771220021/hhttyvq6pdwp40n3olzu.png",
+                "posterId": "698d8264c821712c4b4fd92f",
+                "selectedTopics": ["Technology"],
+                "updated": "2026-02-16T10:30:00Z",
+              },
+              {
+                "_id": "some",
+                "title": "my blog",
+                "content": "this is my blog content",
+                "imageUrl":
+                    "https://res.cloudinary.com/duoenlwuj/image/upload/v1771220021/hhttyvq6pdwp40n3olzu.png",
+                "posterId": "698d8264c821712c4b4fd92f",
+                "selectedTopics": ["Technology"],
+                "updated": "2026-02-16T10:30:00Z",
+              },
+            },
+          },
+        );
+
+        when(
+          () => mockDio.get(any(), data: any(named: "data")),
+        ).thenAnswer((_) async => mockResponse);
+
+        final result = await profileRemoteDatasourceImpl.fetchBlogsByUser();
+
+        expect(result, isA<List<BlogModel>>());
+      });
+
+      test("when status is not 200 throw exception", () async {
+        final mockResponse = Response(
+          requestOptions: RequestOptions(path: ""),
+          statusCode: 500,
+          data: {"message": "error"},
+        );
+
+        when(() => mockDio.get(any())).thenAnswer((_) async => mockResponse);
+
+        final result = profileRemoteDatasourceImpl.fetchBlogsByUser();
 
         expect(result, throwsA(isA<ServerException>()));
       });
